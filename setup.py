59385dd8241d230f48fa118d0ccb281827453506
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-04-12 14:57:31 +0100

    add get_bed_gt from VCF

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="pathogen-profiler",
-	version="1.1",
+	version="1.2",
 	packages=["pathogenprofiler",],
 	license="GPL3",
 	long_description="Pathogen profiling tool",
