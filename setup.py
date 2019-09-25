fd899c9ffa3f13daac0f2cc6edde9e155373250d
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-09-25 11:55:55 +0100

    update version to 1.6.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="pathogen-profiler",
-	version="1.6.0",
+	version="1.6.1",
 	packages=["pathogenprofiler",],
 	license="GPL3",
 	long_description="Pathogen profiling tool",
