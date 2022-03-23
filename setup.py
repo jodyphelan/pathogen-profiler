6bd0b736a0f8c13a62ce90f8dc8faf87134587c9
jodyphelan <jody.phelan@lshtm.ac.uk>
2022-03-23 08:36:16 +0000

    add scripts

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ import setuptools
 setuptools.setup(
 
 	name="pathogen-profiler",
-	version="2.0.0",
+	version="2.0.1",
 	packages=["pathogenprofiler",],
 	license="GPL3",
 	long_description="Pathogen profiling tool",
 	scripts=[
-		'scripts/splitchr.py',
-		'scripts/add_dummy_AD.py',
-		'scripts/pathogen-profiler-get-mutations.py',
-		'scripts/pathogen-profiler-gather.py',
-		'scripts/delly-overlaps.py'
+		'scripts/combine_vcf_variants.py',
+		'scripts/rename_vcf_chrom.py',
+		'scripts/add_dummy_AD.py'
 	]
 )
