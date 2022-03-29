7e5ea479ee57f03c9fa6a10b9133f325cf2ea1cc
jodyphelan <jody.phelan@lshtm.ac.uk>
2022-03-29 16:36:08 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="pathogen-profiler",
-	version="2.0.1",
+	version="2.0.2",
 	packages=["pathogenprofiler",],
 	license="GPL3",
 	long_description="Pathogen profiling tool",
