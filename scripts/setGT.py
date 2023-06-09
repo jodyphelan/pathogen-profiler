6263d8adb0a8b56e0126e5ab4dd4d56bddad237d
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-06-09 17:20:49 +0100

    update setGT to analyse all GTs

--- scripts/setGT.py
+++ scripts/setGT.py
@@ -25,22 +25,22 @@ def main(args):
                     tmp[2] = "."
                     row[i]="".join(tmp)
 
-        uniq_mixed_genotypes = set([x for x in re.findall("[0-9][/|][0-9]",line) if x[0]!=x[2]])
+
         idx = row[8].split(":").index("AD")
-        if len(uniq_mixed_genotypes)>=1:
-            for i in range(9,len(row)):
-                if row[i][:3] not in uniq_mixed_genotypes: continue
-                fmt = row[i].split(":")
-                ad = [int(x) for x in fmt[idx].split(",")]
-                total_ad = sum(ad)
-                if total_ad==0:continue
-                adf = [ad[j]/total_ad for j in range(len(ad))]
-                if max(adf)>=ad_cutoff:
-                    new_gt = adf.index(max(adf))
-                    if alleles[new_gt]=="*": new_gt = "."
-                    fmt[0] = f"{new_gt}/{new_gt}"
-                    something_changed = True
-                    row[i] = ":".join(fmt)
+
+        for i in range(9,len(row)):
+
+            fmt = row[i].split(":")
+            ad = [int(x) for x in fmt[idx].split(",")]
+            total_ad = sum(ad)
+            if total_ad==0:continue
+            adf = [ad[j]/total_ad for j in range(len(ad))]
+            if max(adf)>=ad_cutoff:
+                new_gt = adf.index(max(adf))
+                if alleles[new_gt]=="*": new_gt = "."
+                fmt[0] = f"{new_gt}/{new_gt}"
+                something_changed = True
+                row[i] = ":".join(fmt)
         if something_changed:
             sys.stdout.write("\t".join(row)+"\n")
         else:
