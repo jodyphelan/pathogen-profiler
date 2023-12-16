9a2f07094cc2ad417763517212c3ff0a9653b725
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-12-16 13:27:40 +0000

    fix to scripts

--- scripts/get_genome_regions.py
+++ scripts/get_genome_regions.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import pathogenprofiler as pp
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Get genome chunks')
 parser.add_argument('--fasta',help='Genome accession',required=True)
@@ -10,4 +11,4 @@ args = parser.parse_args()
 
 # Get the genome chunks
 for region in pp.get_genome_chunks(args.fasta,args.num):
-    print(region,region.replace(":","_").replace("-","_"),sep="\t") 
+    sys.stdout.write(region + "\t" + region.replace(":","_").replace("-","_") + "\n") 
